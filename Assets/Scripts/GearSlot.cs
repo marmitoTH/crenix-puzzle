aff865bff857b1cb7dd4c6f1aaea0700f0b48e39
Lucas Rodrigues <lucasrodrigues123@hotmail.com.br>
2021-02-18 10:38:40 -0300

    Created gear slot object

---
+++ Assets/Scripts/GearSlot.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class GearSlot : MonoBehaviour
+{
+	private Gear m_gear;
+
+	public Gear gear
+	{
+		set
+		{
+			m_gear = value;
+		}
+	}
+
+	public bool inUse => m_gear;
+}
