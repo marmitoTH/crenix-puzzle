0d055ff41cb23df0204863dedcfe760f03ee56d4
Lucas Rodrigues <lucasrodrigues123@hotmail.com.br>
2021-02-18 18:00:22 -0300

    Added fixed scale when gear is selected

--- Assets/Scripts/Gear.cs
+++ Assets/Scripts/Gear.cs
@@ -2,52 +2,100 @@
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class Gear : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+public class Gear : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
 {
-	public Sprite gearInUse;
+	public Sprite connectedSprite;
+	public Vector2 dragSize = new Vector2(200, 200);
 
-	private Canvas m_canvas;
+	private Slot m_attachedTo;
+	private Slot m_lastAttachment;
+	private Slot m_initialSlot;
+	
 	private RectTransform m_transform;
 	private CanvasGroup m_canvasGroup;
-	private Transform m_initialParent;
-	private Slot m_slot;
+	private Canvas m_canvas;
 	private Image m_image;
 	private Sprite m_defaultSprite;
 
-	private void Start()
+	private void Awake()
 	{
-		m_canvas = GetComponentInParent<Canvas>();
-		m_transform = GetComponent<RectTransform>();
 		m_image = GetComponent<Image>();
-
-		m_defaultSprite = m_image.sprite;
-		m_initialParent = m_transform.parent;
+		m_transform = GetComponent<RectTransform>();
+		m_canvas = GetComponentInParent<Canvas>();
 
 		if (!TryGetComponent(out m_canvasGroup))
 		{
 			m_canvasGroup = gameObject.AddComponent<CanvasGroup>();
 		}
+
+		m_defaultSprite = m_image.sprite;
+		AttachToParent();
+		m_initialSlot = m_attachedTo;
 	}
 
-	public void Reset()
+	public void AttachToParent()
 	{
-		if (m_slot)
+		var slot = GetComponentInParent<Slot>();
+
+		if (slot)
 		{
-			m_slot.gear = null;
-			m_slot = null;
+			Attach(slot);
+		}
+	}
+
+	public void Attach(Slot to)
+	{
+		if (to && !to.inUse)
+		{
+			m_attachedTo = to;
+			m_attachedTo.gear = this;
+			m_transform.SetParent(m_attachedTo.transform);
+			m_transform.sizeDelta = Vector2.zero;
+			m_transform.anchoredPosition = Vector2.zero;
+		}
+	}
+
+	public void Detach()
+	{
+		if (m_attachedTo)
+		{
+			m_lastAttachment = m_attachedTo;
+			m_attachedTo.gear = null;
+			m_attachedTo = null;
+			SetConnected(false);
 		}
 
-		m_image.sprite = m_defaultSprite;
-		m_transform.SetParent(m_initialParent);
-		m_transform.sizeDelta = Vector2.zero;
+		m_transform.SetParent(m_canvas.transform);
 	}
 
-	public void OnBeginDrag(PointerEventData eventData)
+	public void Restore()
 	{
-		Reset();
-		m_canvasGroup.blocksRaycasts = false;
-		m_transform.SetParent(m_canvas.transform);
+		if (m_lastAttachment)
+		{
+			Attach(m_lastAttachment);
+		}
+	}
+
+	public void ResetState()
+	{
+		if (m_initialSlot)
+		{
+			Attach(m_initialSlot);
+		}
+	}
+
+	public void SetConnected(bool connected)
+	{
+		m_image.sprite = connected ? connectedSprite : m_defaultSprite;
+	}
+
+	public void OnPointerDown(PointerEventData eventData)
+	{
+		Detach();
 		m_transform.position = eventData.position;
+		m_transform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, dragSize.x);
+		m_transform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, dragSize.y);
+		m_canvasGroup.blocksRaycasts = false;
 	}
 
 	public void OnDrag(PointerEventData eventData)
@@ -55,24 +103,19 @@ public class Gear : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHand
 		m_transform.position += (Vector3)eventData.delta;
 	}
 
-	public void OnEndDrag(PointerEventData eventData)
+	public void OnPointerUp(PointerEventData eventData)
 	{
 		var target = eventData.pointerCurrentRaycast.gameObject;
 
-		if (target && target.TryGetComponent<Slot>(out var slot) && !slot.inUse)
+		if (target && target.TryGetComponent<Slot>(out var slot))
 		{
-			m_slot = slot;
-			m_slot.gear = this;
-			m_image.sprite = gearInUse;
-			m_transform.SetParent(slot.transform);
-			m_transform.sizeDelta = Vector2.zero;
-		} 
+			Attach(slot);
+		}
 		else
 		{
-			Reset();
+			Restore();
 		}
 
 		m_canvasGroup.blocksRaycasts = true;
-		m_transform.anchoredPosition = Vector2.zero;
 	}
 }
