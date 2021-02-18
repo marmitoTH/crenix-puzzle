703a039a21594677dce62686dab346239cf4602c
Lucas Rodrigues <lucasrodrigues123@hotmail.com.br>
2021-02-18 17:48:32 -0300

    Created game listener to react to game manager state changes

---
+++ Assets/Scripts/GameListener.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class GameListener : MonoBehaviour
+{
+	public UnityEvent onGameCompleted;
+	public UnityEvent onGameNotCompleted;
+
+	private GameManager m_manager;
+
+	private void Start()
+	{
+		m_manager = GameManager.instance;
+
+		if (m_manager)
+		{
+			m_manager.onPuzzleCompleted.AddListener(() => OnGameCompleted());
+			m_manager.onPuzzleIncompleted.AddListener(() => OnGameNotCompleted());
+		}
+	}
+
+	protected virtual void OnGameCompleted()
+	{
+		onGameCompleted.Invoke();
+	}
+
+	protected virtual void OnGameNotCompleted()
+	{
+		onGameNotCompleted.Invoke();
+	}
+}
