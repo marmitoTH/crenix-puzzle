6c67b13f7882913597feb6bab22a346cdb76423c
Lucas Rodrigues <lucasrodrigues123@hotmail.com.br>
2021-02-18 17:47:22 -0300

    Created game manager object to handle the game state

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine.Events;
+
+public class GameManager : Singleton<GameManager>
+{
+	public UnityEvent onPuzzleCompleted;
+	public UnityEvent onPuzzleIncompleted;
+
+	private Gear[] m_gears;
+	private MotorSlot[] m_motorSlots;
+
+	private bool m_puzzleCompleted;
+
+	private void Start()
+	{
+		m_gears = FindObjectsOfType<Gear>();
+		m_motorSlots = FindObjectsOfType<MotorSlot>();
+	}
+
+	public void ResetPuzzle()
+	{
+		DetachAll();
+		ResetAll();
+		CheckPuzzle();
+	}
+
+	public void CheckPuzzle()
+	{
+		foreach (MotorSlot slot in m_motorSlots)
+		{
+			if (!slot.inUse)
+			{
+				ResetCompletion();
+				return;
+			}
+		}
+
+		CompletePuzzle();
+	}
+
+	private void CompletePuzzle()
+	{
+		if (!m_puzzleCompleted)
+		{
+			m_puzzleCompleted = true;
+			onPuzzleCompleted.Invoke();
+		}
+	}
+
+	private void ResetCompletion()
+	{
+		if (m_puzzleCompleted)
+		{
+			m_puzzleCompleted = false;
+			onPuzzleIncompleted.Invoke();
+		}
+	}
+
+	private void DetachAll()
+	{
+		foreach (Gear gear in m_gears)
+		{
+			gear.Detach();
+		}
+	}
+
+	private void ResetAll()
+	{
+		foreach (Gear gear in m_gears)
+		{
+			gear.ResetState();
+		}
+	}
+}
---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+	private static T m_instance;
+
+	public static T instance
+	{
+		get
+		{
+			if (!m_instance)
+			{
+				m_instance = FindObjectOfType<T>();
+			}
+
+			return m_instance;
+		}
+	}
+}
